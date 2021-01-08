2c06c145f15ec050947d1f678ae9e26648e94f88
Devin Riegle <devin.riegle@joinroot.com>
2021-01-08 00:59:56 -0500

    make header

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Simple Expenses',
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: MyHomePage(title: 'Activity'),
     );
   }
 }
@@ -22,6 +22,48 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class SafeToSpend extends StatelessWidget implements PreferredSizeWidget {
+  final double amount;
+  final Function onPressed;
+
+  const SafeToSpend({
+    Key key,
+    @required this.amount,
+    @required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        margin: const EdgeInsets.only(top: 1),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage('https://picsum.photos/50'),
+                  maxRadius: 10,
+                ),
+                SizedBox(width: 10),
+                Text('Safe-to-Spend', style: TextStyle(color: Colors.white)),
+              ],
+            ),
+            Text('\$${amount.toStringAsFixed(2)}', style: TextStyle(color: Colors.white))
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size(300, 40);
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -31,10 +73,91 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  onHamburgerMenuPress() {
+    print('hamburger menu pressed');
+  }
+
+  onMoreActionPress() {
+    print('More pressed');
+  }
+
+  onSafeToSpendPress(BuildContext ctx) async {
+    return showDialog<void>(
+      context: ctx,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey,
+          titlePadding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+          titleTextStyle: TextStyle(fontSize: 15, color: Colors.black),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Available balance', style: TextStyle(color: Colors.white)),
+              Text('\$14.63', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('- Expenses', style: TextStyle(color: Colors.white.withAlpha(200))),
+                    Text('\$0.00', style: TextStyle(color: Colors.white.withAlpha(200))),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('- Goals', style: TextStyle(color: Colors.white.withAlpha(200))),
+                    Text('\$0.00', style: TextStyle(color: Colors.white.withAlpha(200))),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('- Scheduled', style: TextStyle(color: Colors.white.withAlpha(200))),
+                    Text('\$0.00', style: TextStyle(color: Colors.white.withAlpha(200))),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Safe-to-Spend', style: TextStyle(color: Colors.white)),
+                      Text('\$14.63',
+                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+      appBar: AppBar(
+        backgroundColor: Colors.black.withAlpha(230),
+        title: Text(widget.title),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: onHamburgerMenuPress,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: onMoreActionPress,
+          )
+        ],
+        bottom: SafeToSpend(amount: 0, onPressed: () => onSafeToSpendPress(context)),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
